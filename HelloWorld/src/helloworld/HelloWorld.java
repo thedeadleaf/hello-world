ce402f63f63f1ac5a60582d73886513394510872
Karina Garmendez <garmk6729@wrdsb.ca>
2018-12-19 19:45:51 -0500

    Wrote an altered hello world program.

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!! What is in the sky?");
     }
     
 }
