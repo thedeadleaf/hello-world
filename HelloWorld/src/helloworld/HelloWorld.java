05d330139a0d716b0fd50c86a5f795006caa9f4f
Karina Garmendez <garmk6729@wrdsb.ca>
2018-12-19 19:32:29 -0500

    Wrote a hello world program.

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * Generic 'hello world' program
+ */
+package helloworld;
+
+/**
+ *
+ * @author Karina Garmendez
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
